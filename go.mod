ac43ffc7c52dcba627aed86c570eae2dc5d6ca47
The-Briel-Deal <gabe@fordltc.net>
2022-04-29 23:57:05 -0400

    Implementing heap in go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/The-Briel-Deal/crypto_exchange_backend
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	// "container/heap"
+	"fmt"
+)
+
+// type IntHeap []int
+
+// func (h IntHeap) Len() int				{ return len(h) }
+// func (h IntHeap) Less(i, j int) bool	{ return h[i] < h[j] }
+// func (h IntHeap) Swap(i, j int)			{ h[i], h[j] = h[j], h[i] }
+
+// func (h *IntHeap)
+
+func main() {
+	dog := "silly"
+	fmt.Println("Hello, world.")
+}
